1c1c4e8665d41b8aba29efdd9d772202fe1c12ad
Alex <volkovalex.work@gmail.com>
2019-09-25 23:13:38 -0400

    Task 2 done

--- js/alert.js
+++ js/alert.js
@@ -1,9 +1,7 @@
 let money = prompt("Ваш бюджет на месяц?", ''),
-    time = prompt("Введите дату в формате YYYY-MM-DD", ''),
-    tempA1 = prompt("Введите обязательную статью расходов в этом месяце", '')
-    tempA2 = prompt("Во сколько обойдется", '');
+   time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-let appData = {
+ let appData = {
     budget: money,
     timeData: time,
     expenses: {},
@@ -11,8 +9,47 @@ let appData = {
     income: [],
     savings: false
 };
-appData.expenses.first = tempA1;
-appData.expenses.second = tempA2;
-alert(appData.budget / 30);
+ // Loop 1
+ /*let w =0;
+ let regexNum = /\d/g;
+ do {
+    let first = prompt("Введите обязательную статью расходов в этом месяце", '');
+    let second = prompt("Во сколько обойдется", '');
+    if (typeof (first) === 'string' && first != null && first !="" && first.match(regexNum) == null
+        && typeof (second) === 'string' && second != null && second.match(regexNum) != null){
+       appData.expenses[first] = second;
+    }else {
+       alert("Error");
+    }
+    w++;
+ }while (w<2)*/
+ //Loop2
+/*let regexNum = /\d/g;
+ let  q = 0;
+ while (q <2){
+   console.log(q);
+   let first = prompt("Введите обязательную статью расходов в этом месяце", '');
+   let second = prompt("Во сколько обойдется", '');
+    if (typeof (first) === 'string' && first != null && first !="" && first.match(regexNum) == null
+        && typeof (second) === 'string' && second != null && second.match(regexNum) != null){
+       appData.expenses[first] = second;
+    }else {
+       alert("Error");
+    }
+   q++;
+}*/
+for (let i=0; i<2; i++){
+   let first = prompt("Введите обязательную статью расходов в этом месяце", '');
+   let second = prompt("Во сколько обойдется", '');
+   let regexNum = /\d/g;
 
+   if (typeof (first) === 'string' && first != null && first !="" && first.match(regexNum) == null
+      && typeof (second) === 'string' && second != null && second.match(regexNum) != null){
+      appData.expenses[first] = second;
+   }else {
+      alert("Error");
+   }
+}
+appData.moneyPerDay = appData.budget / 30;
+alert("Money per day: " + appData.moneyPerDay);
 console.log(appData);
