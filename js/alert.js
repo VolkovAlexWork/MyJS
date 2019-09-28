d15617f59fa815fe51677110d0fd9640d8e880c8
Alex <volkovalex.work@gmail.com>
2019-09-28 11:57:25 -0400

    Video done

--- js/alert.js
+++ js/alert.js
@@ -1,55 +1,50 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money, time;
+function start(){
+   money = +prompt("Ваш бюджет на месяц?", '');
    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
+   while (isNaN(money) || money =="" || money == null){
+      money = +prompt("Ваш бюджет на месяц?", '');
+   }
+}
+   start();
  let appData = {
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
- // Loop 1
- /*let w =0;
- let regexNum = /\d/g;
- do {
-    let first = prompt("Введите обязательную статью расходов в этом месяце", '');
-    let second = prompt("Во сколько обойдется", '');
-    if (typeof (first) === 'string' && first != null && first !="" && first.match(regexNum) == null
-        && typeof (second) === 'string' && second != null && second.match(regexNum) != null){
-       appData.expenses[first] = second;
-    }else {
-       alert("Error");
-    }
-    w++;
- }while (w<2)*/
- //Loop2
-/*let regexNum = /\d/g;
- let  q = 0;
- while (q <2){
-   console.log(q);
-   let first = prompt("Введите обязательную статью расходов в этом месяце", '');
-   let second = prompt("Во сколько обойдется", '');
-    if (typeof (first) === 'string' && first != null && first !="" && first.match(regexNum) == null
-        && typeof (second) === 'string' && second != null && second.match(regexNum) != null){
-       appData.expenses[first] = second;
-    }else {
-       alert("Error");
-    }
-   q++;
-}*/
-for (let i=0; i<2; i++){
-   let first = prompt("Введите обязательную статью расходов в этом месяце", '');
-   let second = prompt("Во сколько обойдется", '');
-   let regexNum = /\d/g;
 
-   if (typeof (first) === 'string' && first != null && first !="" && first.match(regexNum) == null
-      && typeof (second) === 'string' && second != null && second.match(regexNum) != null){
-      appData.expenses[first] = second;
-   }else {
-      alert("Error");
+ function chooseExpenses() {
+    for (let i=0; i<2; i++){
+       let first = prompt("Введите обязательную статью расходов в этом месяце", '');
+       let second = prompt("Во сколько обойдется", '');
+       let regexNum = /\d/g;
+
+       if (typeof (first) === 'string' && first != null && first !="" && first.match(regexNum) == null
+           && typeof (second) === 'string' && second != null && second.match(regexNum) != null){
+          appData.expenses[first] = second;
+       }else {
+          alert("Error");
+       }
+    }
+ }
+ chooseExpenses();
+
+ appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+ alert("Money per day: " + appData.moneyPerDay);
+
+ function checkSavings() {
+   if (appData.savings == true){
+      let save = +prompt("Яка сума накоплення?"),
+          percent = +prompt("Під який відсоток?");
+      appData.monthIncom = save/100/12*percent;
+      alert("Доход в місяць з депозиту: " + appData.monthIncom);
    }
 }
-appData.moneyPerDay = appData.budget / 30;
-alert("Money per day: " + appData.moneyPerDay);
-console.log(appData);
+checkSavings();
+
+ console.log(appData);
